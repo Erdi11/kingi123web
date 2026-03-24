1acd9713628a6b699d851ab9c33626ebfe8b77b7
Erdi11 <erdiosmani2019@gmail.com>
2026-03-24 19:20:26 +0100

    12313
    tsillna me kerr ka mahalla veq met bo paranojak :)

---
+++ LESSON26/main.js
@@ -0,0 +1,38 @@
+console.log('Hello Nigga');
+
+
+var x =67 * 6767676767;
+console.log(x);
+
+var a=50;
+var b=10;
+var c=a-b;
+console.log(c);
+
+var n=99;
+n++;
+console.log(n);
+
+var k=200;
+k--;
+console.log(k);
+
+var p=20;
+var p=p ** 2;
+console.log(p);
+
+var y=15;
+y=5;
+console.log(y);
+
+var b1=5;
+var b6="88";
+
+console.log(b1>b6);
+console.log(b1<b6);
+console.log(b1 === b6);
+
+var name = "filian";
+var surname = "Fisteku"
+
+console.log(name+surname);
