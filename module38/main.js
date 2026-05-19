6039520b9510099eb5d099433f67124af3f224df
Erdi11 <erdiosmani2019@gmail.com>
2026-05-19 19:13:24 +0200

    312df

---
+++ module38/main.js
@@ -0,0 +1,19 @@
+// $(function(){
+//     alert("Hello!")
+// })
+
+$('#sort').sortable();
+
+$(function(){
+    $('#accordion').accordion({
+        collapsiable:true
+    });
+});
+    
+$('#tabs').tabs();
+
+$(function(){
+    $('#tabs').tabs({
+        collapsiable:true;
+    });
+});
